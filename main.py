870baab0618753379116029f4bd91a866b4ecaeb
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-13 21:39:45 +0200

    oops

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.include_router(linkroute)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"]
